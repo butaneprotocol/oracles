bb35e3dc755595e1421c56ca3439e24cd2c772ff
Simon Gellis <simongellis@gmail.com>
2024-06-07 23:24:53 -0400

    Shut down network after sleep

--- src/dkg.rs
+++ src/dkg.rs
@@ -175,12 +175,12 @@ pub async fn run(config: &OracleConfig) -> Result<()> {
             KeygenMessage::Done => {
                 done_set.insert(from);
                 if done_set.len() == peers_count {
-                    done_tx.send(()).await.unwrap();
                     info!("All peers have generated their keys!");
                     sleep(Duration::from_secs(3)).await;
                     info!("Press enter to shut down.");
                     part1_broadcast_handle.abort();
                     part2_broadcast_handle.abort();
+                    done_tx.send(()).await.unwrap();
                     spawn_blocking(|| {
                         std::io::stdin().read_line(&mut String::new()).unwrap();
                     })
