72792099f4dee2e1c0feea901389fb21192b0201
Simon Gellis <simongellis@gmail.com>
2026-07-30 14:33:41 -0400

    feat: disable collateral when it has zero weight in proto params

--- src/price_aggregator/synth_config_source.rs
+++ src/price_aggregator/synth_config_source.rs
@@ -11,10 +11,11 @@ use anyhow::{Result, bail};
 use futures::{StreamExt, stream::FuturesUnordered};
 use kupon::MatchOptions;
 use pallas_primitives::{Constr, PlutusData};
+use plutus_parser::AsPlutus;
 
 struct CollateralState {
     config: CollateralConfig,
-    collateral: Option<Vec<String>>,
+    collateral: Option<Vec<Collateral>>,
 }
 
 pub struct SyntheticConfigSource {
@@ -129,20 +130,22 @@ impl SyntheticConfigSource {
                 };
 
                 let mut collateral = vec![];
-                for AssetClass {
-                    policy_id,
-                    asset_name,
-                } in collateral_assets
-                {
-                    if policy_id.is_empty() && asset_name.is_empty() {
-                        collateral.push("ADA".to_string());
+                for (ac, enabled) in collateral_assets {
+                    if ac.policy_id.is_empty() && ac.asset_name.is_empty() {
+                        collateral.push(Collateral {
+                            name: "ADA".to_string(),
+                            enabled,
+                        });
                         continue;
                     }
-                    let asset_id = format!("{policy_id}.{asset_name}");
+                    let asset_id = format!("{}.{}", ac.policy_id, ac.asset_name);
                     let Some(name) = asset_names.get(&asset_id) else {
                         fail!(true, "unrecognized asset id {asset_id}");
                     };
-                    collateral.push(name.clone());
+                    collateral.push(Collateral {
+                        name: name.clone(),
+                        enabled,
+                    });
                 }
 
                 Ok((synthetic, collateral))
@@ -176,12 +179,23 @@ impl SyntheticConfigSource {
         self.next_refresh = now + Duration::from_secs(30);
     }
 
-    pub fn synthetic_collateral(&self, name: &str) -> Option<Vec<String>> {
+    // TODO: report "disabled" collateral entries, make the node just report their prices as 0
+    pub fn synthetic_collateral(&self, name: &str) -> Option<Vec<Collateral>> {
         let state = self.collateral.get(name)?;
         if let Some(collateral) = &state.collateral {
             Some(collateral.clone())
         } else if !state.config.list.is_empty() {
-            Some(state.config.list.clone())
+            Some(
+                state
+                    .config
+                    .list
+                    .iter()
+                    .map(|c| Collateral {
+                        name: c.clone(),
+                        enabled: true,
+                    })
+                    .collect(),
+            )
         } else {
             None
         }
@@ -189,33 +203,25 @@ impl SyntheticConfigSource {
 }
 
 // input is a MonoDatum from the butane Aiken definition
-fn extract_collateral_assets(datum: PlutusData) -> Result<Vec<AssetClass>> {
+fn extract_collateral_assets(datum: PlutusData) -> Result<Vec<(AssetClass, bool)>> {
     // extract the ParamsWrapper
     let [wrapper] = decode_struct(datum, 0)?;
     // extract the LiveParams
     let [params] = decode_struct(wrapper, 0)?;
-    // extract the collateral assets from the LiveParams
-    let [collateral_assets] = decode_struct(params, 0)?;
+    // extract the collateral assets and weights from the LiveParams
+    let [collateral_assets, weights] = decode_struct(params, 0)?;
 
-    let PlutusData::Array(collateral_assets) = collateral_assets else {
-        bail!("datum has invalid collateral assets");
-    };
-
-    let mut assets = vec![];
-    for (index, asset) in collateral_assets.to_vec().into_iter().enumerate() {
-        let [policy_id, asset_name] = decode_struct(asset, 0)?;
-        let PlutusData::BoundedBytes(policy_id) = policy_id else {
-            bail!("asset {index} has invalid policy id");
-        };
-        let PlutusData::BoundedBytes(asset_name) = asset_name else {
-            bail!("asset {index} has invalid asset name");
-        };
-        assets.push(AssetClass {
-            policy_id: policy_id.into(),
-            asset_name: asset_name.into(),
-        });
+    let assets: Vec<AssetClass> = AsPlutus::from_plutus(collateral_assets)?;
+    let weights: Vec<u64> = AsPlutus::from_plutus(weights)?;
+    if assets.len() != weights.len() {
+        bail!(
+            "mismatched number of assets ({}) and weights ({})",
+            assets.len(),
+            weights.len()
+        );
     }
-    Ok(assets)
+    let enabled = weights.iter().map(|w| *w > 0);
+    Ok(assets.into_iter().zip(enabled).collect())
 }
 
 fn decode_struct<const N: usize>(datum: PlutusData, variant: u64) -> Result<[PlutusData; N]> {
@@ -246,7 +252,14 @@ struct UpdateConfigError {
     clear: bool,
 }
 
+#[derive(AsPlutus)]
 struct AssetClass {
     policy_id: String,
     asset_name: String,
+}
+
+#[derive(Clone)]
+pub struct Collateral {
+    pub name: String,
+    pub enabled: bool,
 }
